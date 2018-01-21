7615eddcdc22bf3b74096326e7fc20b69f147264
Jinwoo Song <32620222+jinwoo710@users.noreply.github.com>
2018-01-22 00:24:01 +0900

    resolved #3
    resolved #3 컬렉션 반환형에 대한 최적화

---
+++ GGM.ORM/Exception/ParameterException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GGM.ORM.Exception
+{
+    public enum ParameterError
+    {
+        NotExistType,
+    }
+
+    public class ParameterException : System.Exception
+    {
+        public ParameterException(ParameterError parameterError)
+            : base($"Fail to get parameter. {nameof(ParameterError)} : {parameterError}")
+        {
+            ParameterError = parameterError;
+        }
+
+        public ParameterError ParameterError { get; }
+
+        public static void Check(bool condition, ParameterError parameterError)
+        {
+            if (condition != true)
+                throw new ParameterException(parameterError);
+        }
+    }
+}
+
