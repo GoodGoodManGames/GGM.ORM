3f684aaeadb95a0bc6cce4df683ca6bf2a2bfd73
GoodGoodJM <gjwnsan9420@gmail.com>
2018-02-01 10:41:27 +0900

    Chagne convention and make factory for mysql.

---
+++ GGM.ORM.MySql/MySQLManagerFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.Data.Common;
+using GGM.Application.Attribute;
+using GGM.Context;
+using GGM.Context.Attribute;
+using MySql.Data.MySqlClient;
+
+namespace GGM.ORM.MySql
+{
+    [Managed(ManagedType.Singleton)]
+    public class MySQLManagerFactory : EntityManagerFactory
+    {
+        [Config("ORM.Host")] public string Host { get; set; }
+        [Config("ORM.DataBase")] public string Database { get; set; }
+        [Config("ORM.User")] public string User { get; set; }
+        [Config("ORM.Password")] public string Password { get; set; }
+
+
+        protected override DbConnection CreateDBConnection() =>
+            new MySqlConnection($"Server={Host};Database={Database};Uid={User};pwd={Password};");
+    }
+}
